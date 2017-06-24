f2102545c7373fa76abf8bb7cceb36064476e4dc
Kirin Patel <kirinpatel@gmail.com>
2017-06-24 02:27:25 -0400

    Fixed exporting to jar
    Fixed issue where maven dependencies would not be properly exported.

--- sync/src/com/kirinpatel/tomcat/TomcatServer.java
+++ sync/src/com/kirinpatel/tomcat/TomcatServer.java
@@ -6,6 +6,8 @@ import org.apache.catalina.LifecycleException;
 import org.apache.catalina.Wrapper;
 import org.apache.catalina.startup.Tomcat;
 
+import java.io.File;
+
 /**
  * @author Kirin Patel
  * @date 6/23/17
@@ -15,12 +17,19 @@ public class TomcatServer {
     private Tomcat tomcat;
 
     public TomcatServer() {
+        File mediaPath = new File("./tomcat/webapps/media");
+        if (!mediaPath.getAbsoluteFile().exists()) {
+            Debug.Log("Creating Tomcat file structure...", 1);
+            mediaPath.mkdirs();
+            Debug.Log("Tomcat file structure created.", 1);
+        }
+
         Debug.Log("Starting Tomcat server...", 4);
         tomcat = new Tomcat();
         tomcat.setPort(8080);
         tomcat.setBaseDir("./tomcat");
 
-        Context ctx = tomcat.addContext("", "/");
+        Context ctx = tomcat.addContext("", "/media");
 
         Wrapper defaultServlet = ctx.createWrapper();
         defaultServlet.setName("media");
