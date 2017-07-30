94a985ef8cc7894e1496116dd7b46f712de6a572
Kirin Patel <kirinpatel@gmail.com>
2017-07-30 13:43:08 -0400

    Refactored KeyDispatcher into PlaybackPanel

--- sync/src/com/kirinpatel/gui/PlaybackPanel.java
+++ sync/src/com/kirinpatel/gui/PlaybackPanel.java
@@ -1,7 +1,5 @@
 package com.kirinpatel.gui;
 
-import com.kirinpatel.util.KeyDispatcher;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
@@ -212,6 +210,49 @@ public class PlaybackPanel extends JPanel {
                 mediaPlayer.revalidate();
                 mediaPlayer.repaint();
             }
+        }
+    }
+
+
+    class KeyDispatcher implements KeyEventDispatcher {
+
+        private final PlaybackPanel playbackPanel;
+
+        KeyDispatcher(PlaybackPanel playbackPanel) {
+            this.playbackPanel = playbackPanel;
+        }
+
+        @Override
+        public boolean dispatchKeyEvent(KeyEvent e) {
+            switch(e.getID()) {
+                case KeyEvent.KEY_PRESSED:
+                    switch(e.getKeyCode()) {
+                        case 27:
+                            if (playbackPanel.isFullscreen) {
+                                playbackPanel.closeFullscreen();
+                            }
+                            break;
+                        case 32:
+                            if (playbackPanel.isFullscreen && PlaybackPanel.pauseMedia.isEnabled()) {
+                                if (PlaybackPanel.mediaPlayer.getMedia().isPaused()) PlaybackPanel.mediaPlayer.play();
+                                else PlaybackPanel.mediaPlayer.pause();
+                            }
+                            break;
+                        case 122:
+                            if (playbackPanel.isFullscreen) playbackPanel.closeFullscreen();
+                            else  playbackPanel.initFullscreen();
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case KeyEvent.KEY_RELEASED:
+                    break;
+                default:
+                    break;
+            }
+
+            return false;
         }
     }
 }
