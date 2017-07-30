ef539e55666fddd49afb3fdb8e70323986cadb55
Kirin Patel <kirinpatel@gmail.com>
2017-07-30 13:08:17 -0400

    Concatenated classes to become methods

--- sync/src/com/kirinpatel/Main.java
+++ sync/src/com/kirinpatel/Main.java
@@ -2,18 +2,26 @@ package com.kirinpatel;
 
 import com.kirinpatel.net.*;
 import com.kirinpatel.util.*;
+import jdk.nashorn.api.scripting.URLReader;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * Main class that will run the application and also server as the launcher for the application and primary object for
  * global variables.
  */
 public class Main extends JFrame {
+
+    private final static int VERSION = 1;
+    private final static int BUILD = 5;
+    private final static int REVISION = 0;
 
     // Global variables
     public static boolean showUserTimes = false;
@@ -58,7 +66,7 @@ public class Main extends JFrame {
      * @param args Command line arguments
      */
     public static void main(String[] args) {
-        if (VersionChecker.isUpdated()) {
+        if (isUpdated()) {
             try {
                 UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
             } catch(Exception e) {
@@ -194,6 +202,55 @@ public class Main extends JFrame {
             }
         }
         return null;
+    }
+
+    private static boolean isUpdated() {
+        try {
+            Scanner s = new Scanner(new URLReader(new URL("https://github.com/ajchili/sync/releases")));
+            String version = "";
+            while (s.hasNext()) {
+                String line = s.nextLine();
+                if (line.contains("tag-reference")) {
+                    s.nextLine();
+                    version = s.nextLine();
+                    version = version.substring(version.indexOf("tree/") + 5, version.indexOf("tree/") + 10);
+                    break;
+                }
+            }
+
+            int v = 0;
+            int b = 0;
+            int r = 0;
+
+            for (int i = 0; i < 5; i += 2) {
+                int parsedInt = Integer.parseInt(version.substring(i, i + 1));
+                switch(i) {
+                    case 0:
+                        v = parsedInt;
+                        break;
+                    case 2:
+                        b = parsedInt;
+                        break;
+                    case 4:
+                        r = parsedInt;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            if (VERSION != v) return false;
+            else {
+                if (BUILD < b) return false;
+                else {
+                    if (REVISION < r && BUILD == b) return false;
+                }
+            }
+        } catch(MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        return true;
     }
 
     /**
--- sync/src/com/kirinpatel/util/Media.java
+++ sync/src/com/kirinpatel/util/Media.java
@@ -19,11 +19,12 @@ public class Media implements Serializable {
 
     public Media(Path filePath) {
         this.filePath = filePath.toString();
-        this.url = "_" + URLEncoding.encode(filePath.getFileName().toString());
+        this.url = "_" + encode(filePath.getFileName().toString());
     }
 
     public void setURL(String url) {
         this.url = url;
+        this.filePath = "null";
     }
 
     public void setFilePath(Path filePath) {
@@ -68,5 +69,33 @@ public class Media implements Serializable {
 
     public boolean isPaused() {
         return isPaused;
+    }
+
+
+    /**
+     * Encodes URL to be used for offline media playback.
+     *
+     * Credit: https://www.urlencoder.org/
+     *
+     * @param url Un-encoded URL
+     * @return Returns encoded URL
+     */
+    private static String encode(String url) {
+        url = url.replace("%", "%25");
+        url = url.replace(" ", "%20");
+        url = url.replace("\"", "%22");
+        url = url.replace("-", "%2D");
+        url = url.replace(".", "%2E");
+        url = url.replace("<", "%3C");
+        url = url.replace(">", "%3E");
+        url = url.replace("\\", "%5C");
+        url = url.replace("^", "%5E");
+        url = url.replace("_", "%5F");
+        url = url.replace("`", "%60");
+        url = url.replace("{", "%7B");
+        url = url.replace("|", "%7C");
+        url = url.replace("}", "%7D");
+        url = url.replace("~", "%7E");
+        return url;
     }
 }
