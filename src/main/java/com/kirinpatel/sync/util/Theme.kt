1528f410b0d9c3c6fb3f8de128d229bc6e7d1ad0
Christopher Mills <cltsd27@gmail.com>
2017-11-04 00:06:20 -0400

    Changed static util classes to kotlin

--- src/main/java/com/kirinpatel/sync/util/Theme.kt
+++ src/main/java/com/kirinpatel/sync/util/Theme.kt
@@ -2,22 +2,11 @@ package com.kirinpatel.sync.util
 
 import java.awt.Color
 
-class Theme {
-    companion object {
-        @JvmStatic
-        var isDarkModeEnabled: Boolean = false
-        @JvmStatic
-        val TEXT_STANDARD: Color = Color.BLACK
-        @JvmStatic
-        val TEXT_DARK: Color = Color.LIGHT_GRAY
-        @JvmStatic
-        val BACKGROUND_STANDARD: Color = Color.WHITE
-        @JvmStatic
-        val BACKGROUND_DARK: Color = Color.BLACK
-
-        @JvmStatic
-        fun setIsDarkModeEnabled(isDarkModeEnabled: Boolean) {
-            this.isDarkModeEnabled = isDarkModeEnabled
-        }
-    }
+var isDarkModeEnabled: Boolean = false
+val TEXT_STANDARD: Color = Color.BLACK
+val TEXT_DARK: Color = Color.LIGHT_GRAY
+val BACKGROUND_STANDARD: Color = Color.WHITE
+val BACKGROUND_DARK: Color = Color.BLACK
+fun setIsDarkModeEnabled(darkMode: Boolean) {
+    isDarkModeEnabled = darkMode
 }
