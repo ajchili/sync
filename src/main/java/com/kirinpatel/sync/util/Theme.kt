93f75996bf4153ceaeb86c87d501cb23f6cef176
Kirin Patel <kirinpatel@gmail.com>
2017-11-03 22:26:52 -0400

    Fixed comment

--- src/main/java/com/kirinpatel/sync/util/Theme.kt
+++ src/main/java/com/kirinpatel/sync/util/Theme.kt
@@ -5,10 +5,10 @@ import java.awt.Color
 class Theme {
     companion object {
         var isDarkModeEnabled: Boolean = false
-        var TEXT_STANDARD: Color = Color.BLACK
-        var TEXT_DARK: Color = Color.LIGHT_GRAY
-        var BACKGROUND_STANDARD: Color = Color.WHITE
-        var BACKGROUND_DARK: Color = Color.BLACK
+        val TEXT_STANDARD: Color = Color.BLACK
+        val TEXT_DARK: Color = Color.LIGHT_GRAY
+        val BACKGROUND_STANDARD: Color = Color.WHITE
+        val BACKGROUND_DARK: Color = Color.BLACK
 
         fun setIsDarkModeEnabled(isDarkModeEnabled: Boolean) {
             this.isDarkModeEnabled = isDarkModeEnabled
