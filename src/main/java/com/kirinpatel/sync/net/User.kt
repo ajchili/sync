5706600e0cdc8e7afd0a38aa2e0821dce8a7cc15
Kirin Patel <kirinpatel@gmail.com>
2017-11-03 23:29:35 -0400

    class -> data class

--- src/main/java/com/kirinpatel/sync/net/User.kt
+++ src/main/java/com/kirinpatel/sync/net/User.kt
@@ -3,28 +3,12 @@ package com.kirinpatel.sync.net
 import java.io.Serializable
 import java.util.*
 
-class User constructor(var username: String): Serializable {
+data class User constructor(var username: String): Serializable {
     val userID: Long = Math.abs(Random().nextLong())
     var media: Media.MediaData = Media("").data
     var ping: Long = 0
 
     override fun toString(): String {
         return username
-    }
-
-    override fun equals(other: Any?): Boolean {
-        return if (other is User) {
-            username == other.username && userID == other.userID
-        } else {
-            false
-        }
-    }
-
-    override fun hashCode(): Int {
-        var result = username.hashCode()
-        result = 31 * result + userID.hashCode()
-        result = 31 * result + media.hashCode()
-        result = 31 * result + ping.hashCode()
-        return result
     }
 }
