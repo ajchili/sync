3a47c94cd1df0ecc71dc6b5d82b498982e1b7b64
Kirin Patel <kirinpatel@gmail.com>
2018-02-25 12:17:51 -0500

    Implemented changed suggested by @cltsd27

--- src/main/java/com/kirinpatel/sync/util/DependencyVerifier.java
+++ src/main/java/com/kirinpatel/sync/util/DependencyVerifier.java
@@ -1,70 +1,80 @@
 package com.kirinpatel.sync.util;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
 
 public class DependencyVerifier {
 
     private static final boolean IS_64_BIT = System.getProperty("os.arch").contains("64");
     private static final String OS = System.getProperty("os.name").toLowerCase();
-    private static final String VLC_VERSION = "2.2.8";
+    private static final String[] VLC_DOWNLOAD_LINKS = {
+            "https://ftp.osuosl.org/pub/videolan/vlc/2.2.8/win64/vlc-2.2.8-win64.exe",
+            "https://ftp.osuosl.org/pub/videolan/vlc/2.2.8/macosx/vlc-2.2.8.dmg"
+    };
 
     public static void downloadDependencies() {
-        if (OS.contains("win")) {
-            UIMessage.showMessageDialog("The correct version(s) of Java/VLC were unable to be found on\n" +
-                    "your computer. They will be downloaded and ran for you. Please\n" +
-                    "allow them to be run.", "Downloading dependencies");
-            new Thread(() -> {
-                if (!IS_64_BIT){
-                    try {
-                        runAsAdmin(FileDownloader.downloadFile("Java.exe",
-                                new URL("http://javadl.oracle.com/webapps/download/AutoDL?BundleId=230542_" +
-                                        "2f38c3b165be4555a1fa6e98c45e0808")));
-                    } catch (IOException e) {
-                        UIMessage.showErrorDialog(e, "Unable to download Java");
-                    }
-                }
-            }).start();
+        UIMessage.showMessageDialog("The correct version(s) of Java/VLC were unable to be found on\n" +
+                "your computer. They will be downloaded and ran for you. Please\n" +
+                "allow them to be run.", "Downloading dependencies");
 
-            new Thread(() -> {
+        if (OS.contains("win")) {
+            if (!IS_64_BIT){
                 try {
-                    runAsAdmin(FileDownloader.downloadFile("VLC.exe",
-                            new URL("https://ftp.osuosl.org/pub/videolan/vlc/"
-                                    + VLC_VERSION
-                                    + "/win64/vlc-"
-                                    + VLC_VERSION
-                                    + "-win64.exe")));
+                    runAsAdmin(downloadFile("Java.exe",
+                            new URL("http://javadl.oracle.com/webapps/download/AutoDL?BundleId=230542_" +
+                                    "2f38c3b165be4555a1fa6e98c45e0808")));
                 } catch (IOException e) {
-                    UIMessage.showErrorDialog(e, "Unable to download VLC");
+                    UIMessage.showErrorDialog(e, "Unable to download Java");
                 }
-            }).start();
+            }
+
+            try {
+                runAsAdmin(downloadFile("VLC.exe", new URL(VLC_DOWNLOAD_LINKS[0])));
+            } catch (IOException e) {
+                UIMessage.showErrorDialog(e, "Unable to download VLC");
+            }
         } else if (OS.contains("mac")) {
-            UIMessage.showMessageDialog("The correct version(s) of Java/VLC were unable to be found on\n" +
-                    "your computer. They will be downloaded however, you will\n" +
-                    "have to manually run them.", "Downloading dependencies");
-            new Thread(() -> {
-                try {
-                    runDMG(FileDownloader.downloadFile("VLC.dmg",
-                            new URL("https://mirror.clarkson.edu/videolan/vlc/"
-                                    + VLC_VERSION
-                                    + "/macosx/vlc-"
-                                    + VLC_VERSION
-                                    + ".dmg")));
-                } catch (IOException e) {
-                    UIMessage.showErrorDialog(e, "Unable to download VLC");
-                }
-            }).start();
-        } else if (OS.contains("nix") || OS.contains("nux") || OS.contains("aix")) {
-            UIMessage.showErrorDialog(
-                    new IllegalStateException("Unable to load VLCJ or Java." +
-                            "\nPlease ensure that both VLC and Java are installed and are the same " +
-                            "(32 or 64 bit depending on your system)."),
-                    "Unable to launch sync");
-        } else {
+            try {
+                runDMG(downloadFile("VLC.dmg", new URL(VLC_DOWNLOAD_LINKS[1])));
+            } catch (IOException e) {
+                UIMessage.showErrorDialog(e, "Unable to download VLC");
+            }
+        }
+        /*
+            Linux dependency downloading will be unsupported until a system can be used to test/develop on.
+
+            else if (OS.contains("nix") || OS.contains("nux") || OS.contains("aix")) {
+                UIMessage.showErrorDialog(
+                        new IllegalStateException("Unable to load VLCJ or Java." +
+                                "\nPlease ensure that both VLC and Java are installed and are the same " +
+                                "(32 or 64 bit depending on your system)."),
+                        "Unable to launch sync");
+            }
+        */
+        else {
             UIMessage.showErrorDialog(new IOException("Unable to determine operating system." +
                             "\nYou must manually download dependencies."),
                     "Error downloading dependencies.");
         }
+    }
+
+    @NotNull
+    private static String downloadFile(String name, URL url)throws IOException {
+        try {
+            ReadableByteChannel rbc = Channels.newChannel(url.openStream());
+            FileOutputStream fos = new FileOutputStream(name);
+            fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+            fos.close();
+        } catch (IOException e) {
+            throw new IOException("File unable to download!", e);
+        }
+        return new File(name).getAbsolutePath();
     }
 
     private static void runAsAdmin(String filePath) throws IOException {
