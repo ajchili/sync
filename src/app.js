ce30c39de39780800baa3634769a1c6886176c14
Kirin Patel <kirinpatel@gmail.com>
2018-03-02 21:20:12 -0500

    Setup React + Electron

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+
+render((
+  <div>
+    <h1>Testing</h1>
+  </div>
+), document.getElementById('app'));
---
+++ src/utils/Theme.js
@@ -0,0 +1,14 @@
+import { createMuiTheme } from 'material-ui/styles';
+import indigo from 'material-ui/colors/indigo';
+import pink from 'material-ui/colors/pink';
+import red from 'material-ui/colors/red';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: indigo,
+    secondary: pink,
+    error: red,
+    contrastThreshold: 3,
+    tonalOffset: 0.2,
+  },
+});
