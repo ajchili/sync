be4374af146a8284612e19d76ab08a44a3e3cdfd
Kirin Patel <kirinpatel@gmail.com>
2018-03-28 19:33:57 -0400

    Setup basic home page

--- public/js/public.js
+++ public/js/public.js
@@ -1,26 +1,13 @@
 const ref = firebase.database().ref();
 
-
-ref.child('rooms').once('value').then(function (snapshot) {
-  snapshot.forEach(function (childSnapshot) {
-    console.log(childSnapshot.key)
-  })
-})
-
-function setup() {
-  $('.feedback').on('click', function() {
-    $('.feedbackModal').modal('show');
-  });
-}
-
-function submitFeedback() {
-  var feedback = document.getElementById('feedbackModalInput');
-
-  if (feedback.length > 0) {
-
-  } else {
+function loadServers() {
     
-  }
 }
 
-setup();
+document.getElementById('join').addEventListener('click', function (e) {
+    e.preventDefault();
+
+    $('.ui.sidebar').sidebar('toggle');
+
+    return false;
+})
