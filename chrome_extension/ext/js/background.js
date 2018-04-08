ee479d6e12eb7929dd5576505839b51b1210ae48
Kirin Patel <kirinpatel@gmail.com>
2018-04-07 20:36:42 -0400

    Setup basic version of sync for chrome

--- chrome_extension/ext/js/background.js
+++ chrome_extension/ext/js/background.js
@@ -1,17 +1,28 @@
 var ref = firebase.database().ref().child('chrome');
 var user = { uid: checkForUser() };
+var mediaListener;
+
 setupUser();
 
-chrome.tabs.onUpdated.addListener(function(tabID, changeInfo, tab) {
-    if (tab.url) {
+chrome.tabs.onUpdated.addListener(function (tabID, changeInfo, tab) {
+    if (tab.url != null) {
         if (tab.url.indexOf('https://www.netflix.com/') == 0) {
             setUI();
+
+            if (user.state === 1) {
+                if (tab.url.includes('/watch/')) {
+                    setServerMedia(tab.url);
+                    sendMessageToContent({ func: 'setupMediaListener' })
+                } else {
+                    clearServerMedia();
+                }
+            }
         }
     }
 });
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    switch(request.func) {
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    switch (request.func) {
         case 'setUserName':
             setUserName(request.name);
             break;
@@ -36,15 +47,33 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
         case 'loadServerInfo':
             loadServerInfo();
             break;
+        case 'currentPage':
+            handleCurrentPage(request.page);
+            break;
+        case 'updateMedia':
+            if (user.state === 1) {
+                updateMedia(request.media);
+            }
+            break;
         default:
             break;
     }
 });
 
+function handleCurrentPage(window) {
+    if (user.state === 1) {
+
+    } else if (user.state === 2) {
+        if (user.server != null) {
+            setMediaListener();
+        }
+    }
+}
+
 function sendMessageToContent(message) {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, message, function(response) {});
-      });
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, message, null);
+    });
 }
 
 function checkForUser() {
@@ -59,13 +88,15 @@ function checkForUser() {
 }
 
 function setupUser() {
-    ref.child('users').child(user.uid).once('value').then(function(snapshot) {
+    ref.child('users').child(user.uid).once('value').then(function (snapshot) {
         if (!snapshot.exists()) {
-            ref.child('users').child(user.uid).set({ state: 0, name: "syncer" });
+            ref.child('users').child(user.uid).set({ state: 0, name: 'syncer' });
+        } else if (!snapshot.child('name').exists()) {
+            ref.child('users').child(user.uid).child('name').set('syncer');
         }
     });
 
-    ref.child('users').child(user.uid).on('value', function(snapshot) {
+    ref.child('users').child(user.uid).on('value', function (snapshot) {
         user.name = snapshot.child('name').val();
         user.state = snapshot.child('state').val();
         user.server = snapshot.child('server').val();
@@ -82,10 +113,25 @@ function setUserState(state) {
     ref.child('users').child(user.uid).child('state').set(state);
 }
 
+function setMediaListener() {
+    mediaListener = ref.child('servers').child(user.server).child('media');
+    mediaListener.on('value', function (media) {
+        if (media.exists()) {
+            sendMessageToContent({ func: 'setMedia', media: media });
+        }
+    });
+}
+
 function createServer(title) {
     var key = ref.child('servers').push().key;
-    ref.child('servers').child(key).set({ title: title, isPrivate: false, host: user.uid });
-    ref.child('servers').child(key).child('users').child(key).set(user.uid);
+    ref.child('servers').child(key).set({
+        title: title,
+        isPrivate: false,
+        host: user.uid,
+        users: {
+            host: user.uid
+        }
+    });
     ref.child('users').child(user.uid).child('server').set(key);
     setUserState(1);
 }
@@ -95,15 +141,46 @@ function joinServer(serverKey) {
     ref.child('servers').child(serverKey).child('users').child(key).set(user.uid);
     ref.child('users').child(user.uid).child('server').set(serverKey);
     setUserState(2);
+    setMediaListener();
+}
+
+function setServerMedia(url) {
+    let isTracked = url.includes('?trackId');
+    let media = null;
+
+    if (isTracked) {
+        media = url.substring(url.lastIndexOf('/') + 1, url.indexOf('?'));
+    } else {
+        media = url.substring(url.lastIndexOf('/') + 1);
+    }
+
+    ref.child('servers').child(user.server).child('media').set({
+        title: media,
+        paused: true,
+        time: 0
+    });
+}
+
+function updateMedia(media) {
+    ref.child('servers').child(user.server).child('media').update({
+        paused: media.paused,
+        time: media.time
+    });
+}
+
+function clearServerMedia() {
+    if (user.server != null) {
+        ref.child('servers').child(user.server).child('media').remove();
+    }
 }
 
 function endServer() {
-    ref.child('servers').child(user.server).once('value').then(function(snapshot) {
+    ref.child('servers').child(user.server).once('value').then(function (snapshot) {
         if (snapshot.exists()) {
             var serverId = user.server;
 
             if (user.uid === snapshot.child('host').val()) {
-                snapshot.child('users').forEach(function(childSnapshot) {
+                snapshot.child('users').forEach(function (childSnapshot) {
                     ref.child('users').child(childSnapshot.val()).child('state').set(0);
                     ref.child('users').child(childSnapshot.val()).child('server').remove();
                 });
@@ -115,8 +192,12 @@ function endServer() {
 }
 
 function leaveServer() {
-    ref.child('servers').child(user.server).child('users').once('value').then(function(snapshot) {
-        snapshot.forEach(function(childSnapshot) {
+    if (mediaListener) {
+        mediaListener.off();
+    }
+
+    ref.child('servers').child(user.server).child('users').once('value').then(function (snapshot) {
+        snapshot.forEach(function (childSnapshot) {
             if (user.uid === childSnapshot.val()) {
                 ref.child('servers').child(user.server).child('users').child(childSnapshot.key).remove();
                 ref.child('users').child(user.uid).child('state').set(0);
@@ -131,27 +212,28 @@ function setUI() {
 }
 
 function loadServers() {
-    ref.child('servers').on('value', function(snapshot) {
+    ref.child('servers').on('value', function (snapshot) {
         var servers = [];
 
-        snapshot.forEach(function(childSnapshot) {
+        snapshot.forEach(function (childSnapshot) {
             var key = childSnapshot.key;
             var title = childSnapshot.child('title').val();
             var isPrivate = childSnapshot.child('isPrivate').val();
 
             servers.push({ key: key, title: title, isPrivate: isPrivate });
         });
+
         sendMessageToContent({ func: 'displayServers', servers: servers });
     });
 }
 
 function loadServerInfo() {
-    ref.child('servers').child(user.server).on('value', function(snapshot) {
+    ref.child('servers').child(user.server).on('value', function (snapshot) {
         var server = [];
         var users = [];
 
-        snapshot.child('users').forEach(function(childSnapshot) {
-            ref.child('users').child(childSnapshot.val()).once('value').then(function(childChildSnapshot) {
+        snapshot.child('users').forEach(function (childSnapshot) {
+            ref.child('users').child(childSnapshot.val()).once('value').then(function (childChildSnapshot) {
                 users.push({ id: childSnapshot.val(), name: childChildSnapshot.child('name').val() });
             });
         });
@@ -160,4 +242,3 @@ function loadServerInfo() {
         sendMessageToContent({ func: 'displayServerInfo', server: server });
     });
 }
-    
--- chrome_extension/ext/js/content.js
+++ chrome_extension/ext/js/content.js
@@ -1,5 +1,14 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    switch(request.func) {
+var mediaInterval;
+
+// https://stackoverflow.com/a/46816934
+let script = document.createElement('script');
+script.text = ['const videoPlayer = netflix.appContext.state.playerApp.getAPI().videoPlayer;',
+               'const playerSessionId = videoPlayer.getAllPlayerSessionIds()[0];',
+               'const player = videoPlayer.getVideoPlayerBySessionId(playerSessionId);'].join('\n');
+document.head.appendChild(script);
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    switch (request.func) {
         case 'setUI':
             setUI(request.user);
             break;
@@ -9,29 +18,54 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
         case 'displayServerInfo':
             displayServerInfo(request.server);
             break;
+        case 'setMedia':
+            if (window.location.pathname.includes('/watch/' + request.media.title)) {
+                clearInterval(mediaInterval);
+
+                mediaInterval = setInterval(function() {
+                    setMedia(request.media);
+                }, 100);
+            } else {
+                window.location = 'https://www.netflix.com/watch/' + request.media.title;
+            }
+            break;
+        case 'setupMediaListener':
+            clearInterval(mediaInterval);
+
+            let video = document.getElementsByTagName('video')[0]
+            
+            mediaInterval = setInterval(function() {
+                sendMessageToBackground({ func: 'updateMedia', media: { paused: video.paused, time: video.currentTime } });
+            }, 100);
+            break;
         default:
             break;
     }
 });
 
 function sendMessageToBackground(message) {
-    chrome.runtime.sendMessage(message, function(response) {});
+    chrome.runtime.sendMessage(message, null);
 }
 
+sendMessageToBackground({ func: 'currentPage', page: window.location });
+
 function setUI(user) {
-    document.getElementsByClassName('ArkiaPlayer').classList += 'sync-player';
-    
+    if (document.getElementsByClassName('AkiraPlayer')[0] != null
+        && !document.getElementsByClassName('AkiraPlayer')[0].classList.value.includes('sync-player')) {
+        document.getElementsByClassName('AkiraPlayer')[0].classList.add('sync-player');
+    }
+
     if (!document.getElementById('appMountPoint').classList.contains('sync-netflix')) {
-        document.getElementById('appMountPoint').classList += 'sync-netflix';
+        document.getElementById('appMountPoint').classList.add('sync-netflix');
         document.body.childNodes[1].classList = 'sync';
         document.body.childNodes[1].innerHTML += '<div id="sync"></div>';
     }
 
-    switch(user.state) {
+    switch (user.state) {
         case 0:
-            document.getElementById('sync').innerHTML = '<h3 class="sync-header">sync for Chrome</h3><div class="sync-browser"><ul id="syncServerList"></ul></div><div class="sync-taskbar"><div class="sync-taskbar-button-container"><a id="hostButton" role="link" aria-label="Host"><span tabindex="-1" class="nf-icon-button nf-flat-button nf-flat-button-primary"><span class="nf-flat-button-text">Host</span></span></a><a role="link" aria-label="Join"><span tabindex="-1" class="nf-icon-button nf-flat-button nf-flat-button-primary"><span class="nf-flat-button-text">Join</span></span></a></div></div></div>';
-    
-            document.getElementById('hostButton').onclick = function() {
+            document.getElementById('sync').innerHTML = '<h3 class="sync-header">sync for Chrome</h3><div class="sync-browser"><ul id="syncServerList"></ul></div><div class="sync-taskbar"><div class="sync-taskbar-button-container"><a id="hostButton" role="link" aria-label="Host"><span tabindex="-1" class="nf-icon-button nf-flat-button nf-flat-button-primary"><span class="nf-flat-button-text">Host</span></span></a><a role="link" aria-label="Join" hidden><span tabindex="-1" class="nf-icon-button nf-flat-button nf-flat-button-primary"><span class="nf-flat-button-text">Join</span></span></a></div></div></div>';
+
+            document.getElementById('hostButton').onclick = function () {
                 var title = prompt('Please enter a tile', '');
                 if (title.length > 0) {
                     sendMessageToBackground({ func: 'createServer', title: title });
@@ -45,8 +79,8 @@ function setUI(user) {
             break;
         case 1:
             document.getElementById('sync').innerHTML = '<h3 class="sync-header">sync for Chrome</h3><h4 style="margin-left: 10%;">Users</h4><div class="sync-user-list"><ul id="syncUsersList"></ul></div><div class="sync-taskbar"><div class="sync-taskbar-button-container"><a id="endButton" role="link" aria-label="End"><span tabindex="-1" class="nf-icon-button nf-flat-button nf-flat-button-primary"><span class="nf-flat-button-text">End</span></span></a></div></div></div>';
-    
-            document.getElementById('endButton').onclick = function() {
+
+            document.getElementById('endButton').onclick = function () {
                 sendMessageToBackground({ func: 'endServer' });
             }
 
@@ -54,8 +88,8 @@ function setUI(user) {
             break;
         case 2:
             document.getElementById('sync').innerHTML = '<h3 class="sync-header">sync for Chrome</h3><h4 style="margin-left: 10%;">Users</h4><div class="sync-user-list"><ul id="syncUsersList"></ul></div><div class="sync-taskbar"><div class="sync-taskbar-button-container"><a id="disconnectButton" role="link" aria-label="Disconnect"><span tabindex="-1" class="nf-icon-button nf-flat-button nf-flat-button-primary"><span class="nf-flat-button-text">Disconnect</span></span></a></div></div></div>';
-    
-            document.getElementById('disconnectButton').onclick = function() {
+
+            document.getElementById('disconnectButton').onclick = function () {
                 sendMessageToBackground({ func: 'leaveServer' });
             }
 
@@ -66,24 +100,66 @@ function setUI(user) {
 
 function displayServers(servers) {
     var serverList = document.getElementById('syncServerList');
-    serverList.innerHTML = '';
+    if (serverList != null) {
+        serverList.innerHTML = '';
 
-    servers.forEach(function(server) {
-        if (!server.isPrivate) {
-            serverList.innerHTML += '<li class="server"><h4 class="sync-server-title">' + server.title + '<h4><div class="sync-server-button"><a id="join_' + server.key +'" role="link" aria-label="Join"><span tabindex="-1" class="nf-icon-button nf-flat-button nf-flat-button-primary"><span class="nf-flat-button-text">Join</span></span></a></div></li>';
-    
-            document.getElementById('join_' + server.key).onclick = function() {
-                sendMessageToBackground({ func: 'joinServer', key: server.key });
+        servers.forEach(function (server) {
+            if (server.isPrivate === false) {
+                serverList.insertAdjacentHTML('beforeend', '<li class="server"><h4 class="sync-server-title">' + server.title + '<h4><div class="sync-server-button"><a id="join_' + server.key + '" role="link" aria-label="Join"><span tabindex="-1" class="nf-icon-button nf-flat-button nf-flat-button-primary"><span class="nf-flat-button-text">Join</span></span></a></div></li>');
+
+                document.getElementById('join_' + server.key).onclick = function () {
+                    sendMessageToBackground({ func: 'joinServer', key: server.key });
+                }
+            } else {
+                serverList.insertAdjacentHTML('beforeend', '<li class="server"><h4 class="sync-server-title">' + server.title + '<h4><div class="sync-server-button"></div></li>');
             }
-        }
-    });
+        });
+    }
 }
 
 function displayServerInfo(server) {
     var userList = document.getElementById('syncUsersList');
-    userList.innerHTML = '';
+    if (userList != null) {
+        userList.innerHTML = '';
 
-    server.users.forEach(function(user) {
-        userList.innerHTML += '<li class="user"><h5 class="sync-user-name">' + user.name + '<h5>';
-    });
+        server.users.forEach(function (user) {
+            userList.innerHTML += '<li class="user"><h5 class="sync-user-name">' + user.name + '<h5>';
+        });
+    }
+}
+
+function play() {
+    let script = document.createElement('script');
+    script.text = '(videoPlayer.getVideoPlayerBySessionId(videoPlayer.getAllPlayerSessionIds()[0]).play())();';
+    document.head.appendChild(script);
+}
+
+function pause() {
+    let script = document.createElement('script');
+    script.text = '(videoPlayer.getVideoPlayerBySessionId(videoPlayer.getAllPlayerSessionIds()[0]).pause())();';
+    document.head.appendChild(script);
+}
+
+function seek(place) {
+    let script = document.createElement('script');
+    script.text = '(videoPlayer.getVideoPlayerBySessionId(videoPlayer.getAllPlayerSessionIds()[0]).seek(' + place + '))();';
+    document.head.appendChild(script);
+}
+
+function setMedia(media) {
+    let video = document.getElementsByTagName('video')[0];
+
+    if (video != null) {
+        if (video.paused != media.paused) {
+            if (media.paused) {
+                pause();
+            } else {
+                play();
+            }
+        }
+    
+        if ((video.currentTime < media.time - 1 || video.currentTime > media.time + 1) && video.buffered.length > 0) {
+            seek(media.time);
+        }
+    }
 }
