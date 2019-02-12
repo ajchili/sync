2878b343af4e5ddf5371fe3e56047ea539ce68bf
Kirin Patel <kirinpatel@gmail.com>
2019-02-11 19:11:51 -0500

    Added environment url for development

--- src/electron-starter.js
+++ src/electron-starter.js
@@ -1,5 +1,7 @@
 // @ts-ignore
 const electron = require("electron");
+const path = require("path");
+const url = require("url");
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
@@ -7,7 +9,14 @@ let mainWindow;
 
 function createWindow() {
   mainWindow = new BrowserWindow({ width: 800, height: 600 });
-  mainWindow.loadURL("http://localhost:3000");
+  const page =
+    process.env.ELECTRON_URL ||
+    url.format({
+      pathname: path.join(__dirname, "/../build/index.html"),
+      protocol: "file:",
+      slashes: true
+    });
+  mainWindow.loadURL(page);
 
   mainWindow.webContents.openDevTools();
   mainWindow.on("closed", function() {
