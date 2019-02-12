c49c930faef9b2302e97ec1541b7c2ad1b028d32
Kirin Patel <kirinpatel@gmail.com>
2019-02-11 23:33:18 -0500

    Refactored backend for easier integration and development

--- src/server/index.js
+++ src/server/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use("/", require("./routes"));
+
 // eslint-disable-next-line
 app.listen(port, () => console.log(`Sync listening on port ${port}!`));
---
+++ src/server/routes/index.js
@@ -0,0 +1,9 @@
+// @ts-ignore
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.send("Your sync server is running.");
+});
+
+module.exports = router;
