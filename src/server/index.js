648286997a6abf8237380317fba1aeec235b8beb
Kirin Patel <kirinpatel@gmail.com>
2019-02-12 23:40:57 -0500

    Setup body-parser and cors

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,19 @@
 // @ts-ignore
+const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+// cors
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allo-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use("/", require("./routes"));
 
-// eslint-disable-next-line
 app.listen(port, () => console.log(`Sync listening on port ${port}!`));
