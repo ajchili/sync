24d89771e1f409191ea1079280806112ae8fb821
Kirin Patel <kirinpatel@gmail.com>
2019-02-14 22:00:28 -0500

    Updated Communicator

--- src/services/Communicator.ts
+++ src/services/Communicator.ts
@@ -1,17 +1,22 @@
 import axios from "axios";
 
+const LOCALHOST = "http://localhost:8080";
+
 export default {
   createRoom: async () => {
     let res = await axios({
-      url: "http://localhost:8080/room/create",
+      url: `${LOCALHOST}/room/create`,
       method: "POST"
     });
     return res.data;
   },
   getClientType: async () => {
     try {
-      let res = await axios.get("http://localhost:8080");
-      return res.data === "Your sync server is running." ? "desktop" : "web";
+      let res = await axios({
+        url: LOCALHOST,
+        method: "GET"
+      });
+      return res.status === 200 ? "desktop" : "web";
     } catch (err) {
       return "web";
     }
