51cc67aac80b28f24b85e9747304a3c4fdc29237
Kirin Patel <kirinpatel@gmail.com>
2019-05-02 00:16:58 -0400

    Updates

---
+++ precache-manifest.d31428f797c0443cfe6215254651e877.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "51fb4615d07bb1cf04bf",
+    "url": "/sync/static/css/main.6890f69b.chunk.css"
+  },
+  {
+    "revision": "51fb4615d07bb1cf04bf",
+    "url": "/sync/static/js/main.51fb4615.chunk.js"
+  },
+  {
+    "revision": "79106127993e69a5b465",
+    "url": "/sync/static/js/runtime~main.79106127.js"
+  },
+  {
+    "revision": "3e6060a200e9628573e0",
+    "url": "/sync/static/js/2.3e6060a2.chunk.js"
+  },
+  {
+    "revision": "fcdd2411676520a0ef777d33954609d7",
+    "url": "/sync/index.html"
+  }
+];
