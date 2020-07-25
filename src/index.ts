beab6f12426e820ff8b92e523d936ed3af1ecb89
Kirin Patel <kirinpatel@gmail.com>
2020-07-25 15:00:52 -0400

    Created express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hello, World!');
+import express from 'express';
+import { createServer } from 'http';
+
+const PORT: string = process.env.PORT || '8080';
+
+const app = express();
+
+app.use('/media', express.static('media'));
+
+const server = createServer(app);
+
+server.listen(PORT);
